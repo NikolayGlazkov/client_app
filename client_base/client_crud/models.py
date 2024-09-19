c5acada4e82652095e4be7e6ac727080b52c9a15
NikolayG <gn9377419582@gmail.comm>
2024-09-19 21:14:18 +0300

    fix

--- client_base/client_crud/models.py
+++ client_base/client_crud/models.py
@@ -1,46 +1,70 @@
 from django.db import models
 
-# клинты
+# клиенты
 class Person(models.Model):
-    name = models.CharField(max_length=30,verbose_name="Имя")
-    surname = models.CharField(max_length=30,verbose_name="Отчество")
-    lastname = models.CharField(max_length=30,verbose_name='Фамилия')
+    name = models.CharField(max_length=30, verbose_name="Имя")
+    surname = models.CharField(max_length=30, verbose_name="Отчество")
+    lastname = models.CharField(max_length=30, verbose_name='Фамилия')
     date_of_birth = models.DateField(verbose_name='Дата рождения')
-    place_of_birth = models.CharField(max_length=30,verbose_name='Место рождения')
-    city = models.CharField(max_length=30,verbose_name='город проживания')
-    street = models.CharField(max_length=40,verbose_name='Улица')
-    post_index = models.CharField(max_length=8,verbose_name='Почтовый индекс')
-    email = models.EmailField(max_length=50,verbose_name='Электронная почта')
-    phone_number = models.CharField(max_length=15,verbose_name='Номер телефона')
-    pasport_number = models.CharField(max_length=6,verbose_name='Номер паспорта')
-    pasport_seria = models.CharField(max_length=4,verbose_name='Серия паспорта')
-    issued_by = models.CharField(max_length=100,verbose_name='Место выдачи')
+    place_of_birth = models.CharField(max_length=30, verbose_name='Место рождения')
+    city = models.CharField(max_length=30, verbose_name='Город проживания')
+    street = models.CharField(max_length=40, verbose_name='Улица')
+    post_index = models.CharField(max_length=8, verbose_name='Почтовый индекс')
+    email = models.EmailField(max_length=50, verbose_name='Электронная почта')
+    phone_number = models.CharField(max_length=15, verbose_name='Номер телефона')
+    passport_number = models.CharField(max_length=6, verbose_name='Номер паспорта')
+    passport_seria = models.CharField(max_length=4, verbose_name='Серия паспорта')
+    issued_by = models.CharField(max_length=100, verbose_name='Место выдачи')
     date_of_issue = models.DateField(verbose_name='Дата выдачи')
-    department_code = models.CharField(max_length=7,verbose_name='Код подразделения')
-    inn_number = models.CharField(max_length = 12,verbose_name='Номер ИНН',unique=True)
-    snils_number = models.CharField(max_length = 20,verbose_name='Номер СНИЛС')
+    department_code = models.CharField(max_length=7, verbose_name='Код подразделения')
+    inn_number = models.CharField(max_length=12, verbose_name='Номер ИНН', unique=True)
+    snils_number = models.CharField(max_length=20, verbose_name='Номер СНИЛС')
     tags = models.ManyToManyField('Tag', blank=True, verbose_name='Теги')
-    def __str__ (self) :
-       return self.name
-    
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = "Клиенты"
-        verbose_name = "Клиента"
+        verbose_name = "Клиент"
 
-#теги клиентов (тематика рассылки)
-class Tag(models.Model):
-    name = models.CharField(max_length=20, db_index=True, verbose_name='Теги')
+# класс адреса проживания
+class Address(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='addresses')
+    region = models.CharField(max_length=30, verbose_name="Область")
+    city = models.CharField(max_length=30, verbose_name="Город")
+    district = models.CharField(max_length=30, verbose_name="Район")
+    street = models.CharField(max_length=30, verbose_name="Улица")
+    home_number = models.CharField(max_length=30, verbose_name="Номер дома")
+    apartment_number = models.CharField(max_length=30, verbose_name="Номер квартиры")
 
-    def __str__ (self):
-       return self.name
+    def __str__(self):
+        return self.city
 
     class Meta:
-        verbose_name_plural = 'Тег' 
-        verbose_name = 'Тег' 
+        verbose_name_plural = 'Адреса'
+        verbose_name = 'Адрес'
+        ordering = ['city']
+
+# теги клиентов
+class Tag(models.Model):
+    tag = [
+        ('A', 'Автоперекуп'),
+        ('N', 'Недвижимость'),
+        ('S', 'Спецтехника'),
+        ('F', 'Сельхозтехника'),
+    ]
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Теги', choices=tag)
+
+    def __str__(self):
+        return self.get_name_display()
+
+    class Meta:
+        verbose_name_plural = 'Теги'
+        verbose_name = 'Тег'
         ordering = ['name']
 
-
-#счета клиентов
+# счета клиентов
 class BankAccount(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='bank_accounts')
     account_number = models.CharField(max_length=24, verbose_name='Номер счета')
@@ -50,11 +74,9 @@ class BankAccount(models.Model):
     inn = models.CharField(max_length=15, verbose_name='ИНН банка')
     kpp = models.CharField(max_length=15, verbose_name='КПП банка')
 
+    def __str__(self):
+        return f"Счет {self.account_number} в {self.bank_name}"
+
     class Meta:
         verbose_name_plural = "Банковские счета"
         verbose_name = "Банковский счет"
-
-    def __str__(self):
-        return f"Счет {self.account_number} в {self.bank_name}"
-    
-#теги клиентов
--- client_base/client_crud/views.py
+++ client_base/client_crud/views.py
@@ -4,46 +4,52 @@ from django.shortcuts import render
 from .models import Person,BankAccount,Tag
 from .forms import PerForm
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 class PerCreateView(CreateView):
     template_name = "client_crud/person_create.html"
     form_class = PerForm
     success_url = reverse_lazy('index')
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tags'] = Tag.objects.all()
+        context['page_title'] = 'Создание клиента'
+        context['tags'] = Tag.objects.all()  # Получите все теги из базы данных
         return context
     
 def your_view(request):
     context = {
         'now': timezone.now(),
     }
-    return render(request, 'client_base/client_crud/templates/client_crud/basic.html', context)
+    return render(request, 'client_crud/basic.html', context)
+
 
 
 def index(request):
     context = {
         'page_title': 'Главная',  # Укажите название страницы
     }
-    return render(request,"client_crud/index.html",{"context":context})
+    return render(request, "client_crud/index.html", context)
+
 
 #список клиентов
 def client_list(request):
     persons = Person.objects.all()
-    tag = Tag.objects.all()
-    context = {"persons": persons,"tag":tag}  
-    return render(request,"client_crud/client_list.html",context)
+    tags = Tag.objects.all()  # изменено 'tag' на 'tags'
+    context = {"persons": persons, "tags": tags}  
+    return render(request, "client_crud/client_list.html", context)
+
 
 
 
 # вывод по тегам и тегов
-def by_tag(request,tag_id):
-    clients = Person.objects.filter(tags=tag_id)
+
+def by_tag(request, tag_id):
+    current_tag = get_object_or_404(Tag, pk=tag_id)
+    clients = Person.objects.filter(tags=current_tag)
     tags = Tag.objects.all()
-    current_tag = Tag.objects.get(pk=tag_id)
-    context = {"clients":clients,"tags":tags,"current_tag":current_tag}
-    return render(request,'client_crud/by_tag.html', context)
+    context = {"clients": clients, "tags": tags, "current_tag": current_tag}
+    return render(request, 'client_crud/by_tag.html', context)
 
 
-# создание клиента в форме
+# def add_adres
+# # создание клиента в форме
